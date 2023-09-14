5ef0af4bc9c38fb5dc399536ab56ccd8c6052676
Brennez <tchalisantos40@gmail.com>
2023-09-14 17:49:14 -0300

    feat: aplying animation on expand form

--- shop/lib/components/auth_form.dart
+++ shop/lib/components/auth_form.dart
@@ -15,7 +15,8 @@ class AuthForm extends StatefulWidget {
   State<AuthForm> createState() => _AuthFormState();
 }
 
-class _AuthFormState extends State<AuthForm> {
+class _AuthFormState extends State<AuthForm>
+    with SingleTickerProviderStateMixin {
   AuthMode _authMode = AuthMode.Login;
   final _passwordFocus = FocusNode();
   final _formKey = GlobalKey<FormState>();
@@ -28,8 +29,37 @@ class _AuthFormState extends State<AuthForm> {
     'senha': '',
   };
 
+  AnimationController? _controller;
+  Animation<Size>? _heightAnimation;
+
   bool isSignUp() => _authMode == AuthMode.SignUp;
   bool isLogin() => _authMode == AuthMode.Login;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 300,
+      ),
+    );
+    _heightAnimation = Tween(
+      begin: const Size(double.infinity, 310),
+      end: const Size(double.infinity, 400),
+    ).animate(CurvedAnimation(
+      parent: _controller!,
+      curve: Curves.linear,
+    ));
+
+    // _heightAnimation?.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller?.dispose();
+  }
 
   void _showErrorDialog(String msg) {
     showDialog(
@@ -87,8 +117,10 @@ class _AuthFormState extends State<AuthForm> {
     setState(() {
       if (isLogin()) {
         _authMode = AuthMode.SignUp;
+        _controller?.forward();
       } else {
         _authMode = AuthMode.Login;
+        _controller?.reverse();
       }
     });
   }
@@ -101,89 +133,97 @@ class _AuthFormState extends State<AuthForm> {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Container(
-        height: isLogin() ? 310 : 400,
-        width: deviceSize.width * 0.80,
-        padding: const EdgeInsets.all(16),
+      child: AnimatedBuilder(
+        animation: _heightAnimation!,
+        builder: (context, childForm) {
+          return Container(
+            // height: isLogin() ? 310 : 400,
+            height: _heightAnimation?.value.height ?? (isLogin() ? 310 : 400),
+            width: deviceSize.width * 0.80,
+            padding: const EdgeInsets.all(16),
+            child: childForm,
+          );
+        },
         child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  onFieldSubmitted: (value) =>
-                      FocusScope.of(context).requestFocus(_passwordFocus),
-                  decoration: const InputDecoration(label: Text('E-mail')),
-                  keyboardType: TextInputType.emailAddress,
-                  onSaved: (email) => _authData['email'] = email ?? '',
-                  validator: (_email) {
-                    String email = _email ?? '';
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                onFieldSubmitted: (value) =>
+                    FocusScope.of(context).requestFocus(_passwordFocus),
+                decoration: const InputDecoration(label: Text('E-mail')),
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (email) => _authData['email'] = email ?? '',
+                validator: (_email) {
+                  String email = _email ?? '';
 
-                    if (email.trim().isEmpty || !email.trim().contains('@')) {
-                      return 'Informe um e-mail válido!';
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
+                  if (email.trim().isEmpty || !email.trim().contains('@')) {
+                    return 'Informe um e-mail válido!';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              TextFormField(
+                focusNode: _passwordFocus,
+                controller: _passwordController,
+                decoration: const InputDecoration(label: Text('Senha')),
+                keyboardType: TextInputType.emailAddress,
+                obscureText: true,
+                onSaved: (password) => _authData['password'] = password ?? '',
+                validator: (_password) {
+                  String password = _password ?? '';
+
+                  if (password.trim().isEmpty || password.length < 6) {
+                    return 'Informe uma senha válida!';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              if (isSignUp())
                 TextFormField(
-                  focusNode: _passwordFocus,
-                  controller: _passwordController,
-                  decoration: const InputDecoration(label: Text('Senha')),
+                  decoration:
+                      const InputDecoration(label: Text('Confirme a senha')),
                   keyboardType: TextInputType.emailAddress,
                   obscureText: true,
-                  onSaved: (password) => _authData['password'] = password ?? '',
-                  validator: (_password) {
-                    String password = _password ?? '';
+                  validator: isLogin()
+                      ? null
+                      : (_password) {
+                          String password = _password ?? '';
 
-                    if (password.trim().isEmpty || password.length < 6) {
-                      return 'Informe uma senha válida!';
-                    } else {
-                      return null;
-                    }
-                  },
+                          if (_passwordController.text != password) {
+                            return 'As senhas são diferentes!';
+                          } else {
+                            return null;
+                          }
+                        },
                 ),
-                if (isSignUp())
-                  TextFormField(
-                    decoration:
-                        const InputDecoration(label: Text('Confirme a senha')),
-                    keyboardType: TextInputType.emailAddress,
-                    obscureText: true,
-                    validator: isLogin()
-                        ? null
-                        : (_password) {
-                            String password = _password ?? '';
-
-                            if (_passwordController.text != password) {
-                              return 'As senhas são diferentes!';
-                            } else {
-                              return null;
-                            }
-                          },
-                  ),
-                const SizedBox(
-                  height: 20,
-                ),
-                if (_isLoading)
-                  CircularProgressIndicator()
-                else
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 8, horizontal: 30)),
-                    onPressed: _submit,
-                    child: Text(
-                      isLogin() ? 'LOGIN' : 'CADASTRAR',
-                    ),
-                  ),
-                const Spacer(),
-                TextButton(
-                  onPressed: () => _switchMode(),
+              const SizedBox(
+                height: 20,
+              ),
+              if (_isLoading)
+                CircularProgressIndicator()
+              else
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 30)),
+                  onPressed: _submit,
                   child: Text(
-                    isLogin() ? 'CRIAR CONTA?' : 'JÁ POSSUI CONTA?',
+                    isLogin() ? 'LOGIN' : 'CADASTRAR',
                   ),
-                )
-              ],
-            )),
+                ),
+              const Spacer(),
+              TextButton(
+                onPressed: () => _switchMode(),
+                child: Text(
+                  isLogin() ? 'CRIAR CONTA?' : 'JÁ POSSUI CONTA?',
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
