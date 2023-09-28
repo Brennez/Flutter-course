f08bd7a0b3f916f896c403ad2313a9f4b35b41d3
Brennez <tchalisantos40@gmail.com>
2023-09-28 16:15:55 -0300

    feat: chat page created

--- chat/lib/pages/chat_page.dart
+++ chat/lib/pages/chat_page.dart
@@ -1,6 +1,7 @@
+import 'package:chat/components/messages.dart';
+import 'package:chat/components/new_message.dart';
+import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
-
-import '../core/services/auth/auth_service.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -8,22 +9,62 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'chat page',
-              style: TextStyle(color: Colors.black),
-            ),
-            TextButton(
-                onPressed: () {
+        backgroundColor: Theme.of(context).canvasColor,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          title: Text(
+            'My Chat',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          elevation: 4,
+          centerTitle: true,
+          actions: [
+            DropdownButton(
+              icon: const Icon(
+                Icons.more_vert_rounded,
+                color: Colors.white,
+              ),
+              underline: Text(''),
+              items: [
+                DropdownMenuItem(
+                  value: 'logout',
+                  child: Container(
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.exit_to_app,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Sair',
+                          style:
+                              Theme.of(context).textTheme.labelSmall!.copyWith(
+                                    color: Colors.white,
+                                  ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+              onChanged: (value) {
+                if (value == 'logout') {
                   AuthService().logout();
-                },
-                child: Text('Logout'))
+                }
+              },
+            )
           ],
         ),
-      ),
-    );
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(child: Messages()),
+              NewMessage(),
+            ],
+          ),
+        ));
   }
 }
--- chat/lib/utils/theme.dart
+++ chat/lib/utils/theme.dart
@@ -1,35 +1,37 @@
 import 'package:flutter/material.dart';
 
 ThemeData lightTheme = ThemeData(
-  primaryColor: Color(0xff422B6D),
-  canvasColor: Colors.black54,
-  fontFamily: 'RobotoMono',
-  useMaterial3: true,
-  //
-  elevatedButtonTheme: ElevatedButtonThemeData(
-    style: ElevatedButton.styleFrom(
-      elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
+    primaryColor: Color(0xff422B6D),
+    canvasColor: Color(0xff262626),
+    fontFamily: 'RobotoMono',
+    useMaterial3: true,
+    //
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        backgroundColor: Color(0xff422B6D),
       ),
-      backgroundColor: Color(0xff422B6D),
     ),
-  ),
-  //
-  textTheme: const TextTheme(
-    bodyMedium: TextStyle(
+    //
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(
+        color: Colors.white,
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+      ),
+      labelLarge: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.w600,
+      ),
+      labelSmall: TextStyle(
+        color: Color(0xff422B6D),
+        fontSize: 14,
+        fontWeight: FontWeight.w600,
+      ),
+    ),
+    iconTheme: IconThemeData(
       color: Colors.white,
-      fontSize: 18,
-      fontWeight: FontWeight.w600,
-    ),
-    labelLarge: TextStyle(
-      color: Colors.white,
-      fontWeight: FontWeight.w600,
-    ),
-    labelSmall: TextStyle(
-      color: Color(0xff422B6D),
-      fontSize: 14,
-      fontWeight: FontWeight.w600,
-    ),
-  ),
-);
+    ));
