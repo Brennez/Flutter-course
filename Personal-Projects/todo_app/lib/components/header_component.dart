68131b011499f9adb1c3d3e4e1d1babedd9b3290
Brennez <tchalisantos40@gmail.com>
2023-09-28 12:11:38 -0300

    feat: date formated to pt_br

--- Personal-Projects/todo_app/lib/components/header_component.dart
+++ Personal-Projects/todo_app/lib/components/header_component.dart
@@ -11,11 +11,13 @@ class HeaderComponent extends StatelessWidget {
 // 'March 9, 2020',
   @override
   Widget build(BuildContext context) {
-    // TODO: criar uma função para deixar o dia antes do Mês e concatenar com o restante da String
-    // Por exemplo: 26 Setembro, 2023
+    // TODO: extrair a lógica de formatação para uma função
+    String dayFormated = DateFormat('d ').format(DateTime.now());
+
     String dateFormated =
-        DateFormat('MMMM d, yyyy', 'pt_BR').format(DateTime.now());
-    dateFormated = dateFormated[0].toUpperCase() + dateFormated.substring(1);
+        DateFormat('MMMM, yyyy', 'pt_BR').format(DateTime.now());
+    dateFormated =
+        dayFormated + dateFormated[0].toUpperCase() + dateFormated.substring(1);
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
