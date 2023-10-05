57b64e372a3aa8b6cf46007ecbe39277d464eb1c
Brennez <tchalisantos40@gmail.com>
2023-10-05 17:29:30 -0300

    feat: geting messagens from firestore on firebase

--- chat/lib/core/services/chat/chat_firebase_service.dart
+++ chat/lib/core/services/chat/chat_firebase_service.dart
@@ -1,42 +1,106 @@
 import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../../models/chat_message.dart';
 import '../../models/chat_user.dart';
-import 'package:chat/core/services/chat/chat_service.dart';
+import '../../services/chat/chat_service.dart';
 
 class ChatFirebaseService implements ChatService {
   @override
   Stream<List<ChatMessage>> messagesStream() {
-    return Stream<List<ChatMessage>>.empty();
+    final store = FirebaseFirestore.instance;
+
+    final snapshot = store
+        .collection('chat')
+        .withConverter(fromFirestore: _fromFirestore, toFirestore: _toFirestore)
+        .snapshots();
+
+    return Stream<List<ChatMessage>>.multi((controller) {
+      snapshot.listen(
+        (querySnapshot) {
+          List<ChatMessage> messages = querySnapshot.docs.map((doc) {
+            return doc.data();
+          }).toList();
+
+          controller.add(messages);
+        },
+      );
+    });
   }
 
   @override
   Future<ChatMessage?> save(String text, ChatUser user) async {
     final store = FirebaseFirestore.instance;
 
-    // ChatUser => Map<String, dynamic>
-    final docRef = await store.collection('chat').add({
-      'text': text,
-      'createdAt': DateTime.now().toIso8601String(),
-      'userId': user.id,
-      'userName': user.name,
-      'userImageUrl': user.imageUrl,
-    });
+    final msg = ChatMessage(
+      id: '',
+      text: text,
+      createdAt: DateTime.now(),
+      userId: user.id,
+      userName: user.name,
+      userImageUrl: user.imageUrl,
+    );
 
-    // Map<String, dynamic> => ChatUser
+    final docRef = await store
+        .collection('chat')
+        .withConverter(fromFirestore: _fromFirestore, toFirestore: _toFirestore)
+        .add(msg);
+
     final documentSnapshot = await docRef.get();
 
-    final data = documentSnapshot.data()!;
+    return documentSnapshot.data();
+  }
 
+  // Map<String, dynamic> => ChatMessage
+  ChatMessage _fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> doc, SnapshotOptions? options) {
     return ChatMessage(
-      id: documentSnapshot.id,
-      text: data['text'],
-      createdAt: DateTime.parse(data['createdAt']),
-      userId: data['userId'],
-      userName: data['userName'],
-      userImageUrl: data['userImageUrl'],
+      id: doc.id,
+      text: doc['text'],
+      createdAt: DateTime.parse(doc['createdAt']),
+      userId: doc['userId'],
+      userName: doc['userName'],
+      userImageUrl: doc['userImageUrl'],
     );
   }
+
+  Map<String, dynamic> _toFirestore(
+      ChatMessage chatMessage, SetOptions? options) {
+    return {
+      'text': chatMessage.text,
+      'createdAt': chatMessage.createdAt.toIso8601String(),
+      'userId': chatMessage.userId,
+      'userName': chatMessage.userName,
+      'userImageUrl': chatMessage.userImageUrl,
+    };
+  }
 }
+
+// Implementação sem utilizar os conversores _toFirestore _fromFirestore
+  // @override
+  // Future<ChatMessage?> save(String text, ChatUser user) async {
+  //   final store = FirebaseFirestore.instance;
+
+  //   // ChatMessage => Map<String, dynamic>
+  //   final docRef = await store.collection('chat').add({
+  //     'text': text,
+  //     'createdAt': DateTime.now().toIso8601String(),
+  //     'userId': user.id,
+  //     'userName': user.name,
+  //     'userImageUrl': user.imageUrl,
+  //   });
+
+  //   // Map<String, dynamic> => ChatMessage
+  //   final documentSnapshot = await docRef.get();
+
+  //   final data = documentSnapshot.data()!;
+
+  //   return ChatMessage(
+  //     id: documentSnapshot.id,
+  //     text: data['text'],
+  //     createdAt: DateTime.parse(data['createdAt']),
+  //     userId: data['userId'],
+  //     userName: data['userName'],
+  //     userImageUrl: data['userImageUrl'],
+  //   );
+  // }
