d36395ebc1ea4cac8a4313948188998dd60bc99f
Brennez <tchalisantos40@gmail.com>
2023-11-07 20:46:42 -0300

    fix: fixing overflow error on open keyboard

--- Personal-Projects/todo_app/lib/screens/home_page.dart
+++ Personal-Projects/todo_app/lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/components/header_component.dart';
+import 'package:todo_app/components/todos_component.dart';
 import 'package:todo_app/providers/todo_provider.dart';
 
 import '../components/task_component.dart';
@@ -51,6 +52,7 @@ class _HomePageState extends State<HomePage> {
 
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: kBackgroundColor,
         body: LayoutBuilder(
           builder: (context, constraints) {
@@ -99,94 +101,19 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                     if (todoProvider.todosList.isNotEmpty)
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: constraints.maxHeight * 0.01,
-                          ),
-                          SizedBox(
-                            height: constraints.maxHeight * 0.03,
-                            child: Text(
-                              'Incompletas',
-                              style: myTheme.textTheme.bodyLarge,
-                            ),
-                          ),
-                          SizedBox(
-                            height: constraints.maxHeight * 0.01,
-                          ),
-                          SizedBox(
-                            height: constraints.maxHeight *
-                                (todoProvider.todosIsDoneList.isEmpty
-                                    ? .76
-                                    : .35),
-                            child: ListView.builder(
-                              itemCount: todoProvider.todosList.length,
-                              itemBuilder: (context, index) {
-                                return Dismissible(
-                                  onDismissed: (_) {
-                                    todoProvider.removeTodo(index);
-                                  },
-                                  confirmDismiss: (_) =>
-                                      _showConfirmationModal(),
-                                  direction: DismissDirection.endToStart,
-                                  background: Container(
-                                    decoration: BoxDecoration(
-                                        color: Colors.red,
-                                        borderRadius:
-                                            BorderRadius.circular(20)),
-                                    alignment: Alignment.centerRight,
-                                    child: const Padding(
-                                      padding: EdgeInsets.only(right: 10),
-                                      child: Icon(
-                                        Icons.delete,
-                                        size: 30,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ),
-                                  key: ValueKey<int>(index),
-                                  child: TaskComponent(
-                                    todo: todoProvider.todosList[index],
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                        ],
+                      TodosComponent(
+                        constraints: constraints,
+                        label: "Incompletas",
+                        todoProvider: todoProvider,
+                        isDoneTodos: false,
                       ),
                     if (todoProvider.todosIsDoneList.isNotEmpty)
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: constraints.maxHeight * 0.01,
-                          ),
-                          SizedBox(
-                            height: constraints.maxHeight * 0.03,
-                            child: Text(
-                              'Completas',
-                              style: myTheme.textTheme.bodyLarge,
-                            ),
-                          ),
-                          SizedBox(
-                            height: constraints.maxHeight * 0.01,
-                          ),
-                          SizedBox(
-                            height: todoProvider.todosList.isEmpty
-                                ? constraints.maxHeight * .76
-                                : constraints.maxHeight * .35,
-                            child: ListView.builder(
-                              itemCount: todoProvider.todosIsDoneList.length,
-                              itemBuilder: (context, index) {
-                                return TaskComponent(
-                                  todo: todoProvider.todosIsDoneList[index],
-                                );
-                              },
-                            ),
-                          )
-                        ],
-                      )
+                      TodosComponent(
+                        constraints: constraints,
+                        label: "Completas",
+                        todoProvider: todoProvider,
+                        isDoneTodos: true,
+                      ),
                   ],
                 ),
               ),
